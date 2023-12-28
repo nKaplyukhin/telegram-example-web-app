f89b8f3662af1609b2b7bc3347d92fa41af4fc89
n_kaplyukhin <Nikita.Kaplyukhin@rusal.com>
2023-12-28 16:24:13 +0700

    fix

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
-console.log(window.Telegram);
+document.addEventListener("DOMContentLoaded", () => {
+  let tg = window.Telegram.WebApp;
 
-let tg = window.Telegram.WebApp;
+  tg.MainButton.isVisible = true;
 
-tg.MainButton.isVisible = true;
+  tg.MainButton.text = "Войти";
 
-tg.MainButton.text = "Войти";
+  tg.MainButton.onClick(() => {
+    const form = document.querySelector(".form");
+    const inputs = form.querySelectorAll(".form__input");
 
-tg.MainButton.onClick(() => {
-  const form = document.querySelector(".form");
-  const inputs = form.querySelectorAll(".form__input");
+    const inputsValues = Object.values(inputs).reduce(
+      (acc, item) => ({ ...acc, [item.name]: item.value }),
+      {}
+    );
 
-  const inputsValues = Object.values(inputs).reduce(
-    (acc, item) => ({ ...acc, [item.name]: item.value }),
-    {}
-  );
+    tg.sendData(JSON.stringify(inputsValues));
+    tg.close();
+  });
 
-  tg.sendData(JSON.stringify(inputsValues));
-  tg.close();
+  // document.querySelector(".test").addEventListener("click", (e) => {
+  //   console.log(e);
+  //   const form = document.querySelector(".form");
+  //   const inputs = form.querySelectorAll(".form__input");
+
+  //   const inputsValues = Object.values(inputs).reduce(
+  //     (acc, item) => ({ ...acc, [item.name]: item.value }),
+  //     {}
+  //   );
+
+  //   tg.sendData(JSON.stringify(inputsValues));
+  //   tg.close();
+  // });
 });
-
-// document.querySelector(".test").addEventListener("click", (e) => {
-//   console.log(e);
-//   const form = document.querySelector(".form");
-//   const inputs = form.querySelectorAll(".form__input");
-
-//   const inputsValues = Object.values(inputs).reduce(
-//     (acc, item) => ({ ...acc, [item.name]: item.value }),
-//     {}
-//   );
-
-//   tg.sendData(JSON.stringify(inputsValues));
-//   tg.close();
-// });
